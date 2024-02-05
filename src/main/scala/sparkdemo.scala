31a82d134ad6d4aedccaab21379e22170f540851
satarupa <satrupapanda1@gmail.com>
2024-02-05 21:03:56 +0530

    Rdd program for wordcount

--- src/main/scala/sparkdemo.scala
+++ src/main/scala/sparkdemo.scala
@@ -6,7 +6,7 @@ object sparkdemo {
     val sc=new SparkContext("local[*]","Spark_first_program")
 
     val rdd1=sc.textFile("C:/Users/satar/OneDrive/Desktop/sparktext.txt")
-    val rdd2=rdd1.flatMap(x=>x.split(" "));
+    val rdd2=rdd1.flatMap(x=>x.split(" "))
     val rdd3=rdd2.map(x=>(x,1))
     val rdd4=rdd3.reduceByKey((x,y)=>x+y)
 
